5f83615f21a8a745c8f43f18c23752bb3a99c812
Chenchaiah23 <chen.java23@gmail.com>
2019-02-25 14:09:50 +0530

    Update App.java

--- src/main/java/com/mkyong/App.java
+++ src/main/java/com/mkyong/App.java
@@ -12,7 +12,7 @@ public class App
     public static void main( String[] args )
     {
         App obj = new App();
-        System.out.println("Unique ID112 : " + obj.generateUniqueKey());
+        System.out.println("Unique ID1121 : " + obj.generateUniqueKey());
     }
     
     public String generateUniqueKey(){
